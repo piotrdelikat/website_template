47887063239a2d374cab7633ebec98cb32861fad
Piotr Delikat <piotrdellikat@gmail.com>
2017-12-13 23:48:25 +0100

    index.html and config files added

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/main.js',
+    output: {
+      path: __dirname + '/dist',
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+              {
+                  test:/\.js$/,
+                  exclude: /(node_modules)/,
+                  use: {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: ['es2015']
+                      }
+                  }
+              },
+              {
+                  test:/\.css$/,
+                  use: ExtractTextPlugin.extract({
+                          fallback:'style-loader',
+                          use:['css-loader'],
+                  })
+              },
+              {
+                  test: /\.(eot|svg|ttf|woff|woff2)$/,
+                  loader: "file-loader",
+                  options: {
+                    name: "fonts/[name].[ext]",
+                  },
+              },
+            ]
+          },
+      plugins: [
+         new webpack.ProvidePlugin({
+           $: 'jquery',
+           jQuery: 'jquery',
+           'window.jQuery': 'jquery',
+           Popper: ['popper.js', 'default'],
+           }),
+         new ExtractTextPlugin("bundled_styles.css"),
+  ]
+}

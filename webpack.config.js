3acb788a09ede055526f9a3abf219aecaf075791
Piotr Delikat <piotrdellikat@gmail.com>
2017-12-14 23:28:05 +0100

    Added SCSS support

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,10 @@ module.exports = {
                   }
               },
               {
-                  test:/\.css$/,
+                  test:/\.(s*)css$/,
                   use: ExtractTextPlugin.extract({
                           fallback:'style-loader',
-                          use:['css-loader'],
+                          use:['css-loader','sass-loader'],
                   })
               },
               {
